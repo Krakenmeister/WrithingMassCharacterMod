09ccdb2574575bb47d5830c868cae3c0c6be7e35
Krakenmeister <johndmeo@gmail.com>
2025-07-09 16:28:26 -0400

    Added Roleplay

--- src/main/java/writhingmasscharactermod/actions/RoleplayAction.java
+++ src/main/java/writhingmasscharactermod/actions/RoleplayAction.java
@@ -2,17 +2,13 @@ package writhingmasscharactermod.actions;
 
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.DamageAction;
-import com.megacrit.cardcrawl.actions.common.HealAction;
 import com.megacrit.cardcrawl.cards.DamageInfo;
-import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
-import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.ui.panels.EnergyPanel;
 
 public class RoleplayAction extends AbstractGameAction {
-    public int[] multiDamage;
     private boolean freeToPlayOnce = false;
     private AbstractCreature source;
     private AbstractCreature target;
