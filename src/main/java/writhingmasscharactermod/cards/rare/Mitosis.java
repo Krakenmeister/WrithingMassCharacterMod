0bc5a4ea96d4dbe549b85cc5734f1be7c3f55939
Krakenmeister <johndmeo@gmail.com>
2025-07-10 11:43:49 -0400

    Added Mitosis

---
+++ src/main/java/writhingmasscharactermod/cards/rare/Mitosis.java
@@ -0,0 +1,40 @@
+package writhingmasscharactermod.cards.rare;
+
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.actions.common.HealAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import writhingmasscharactermod.actions.AddRealTemporaryHPAction;
+import writhingmasscharactermod.cards.BaseCard;
+import writhingmasscharactermod.character.WrithingMassCharacter;
+import writhingmasscharactermod.patches.realtemphp.RealTempHPField;
+import writhingmasscharactermod.util.CardStats;
+
+public class Mitosis extends BaseCard {
+    public static final String ID = makeID("Mitosis");
+    private static final CardStats info = new CardStats(
+            WrithingMassCharacter.Meta.CARD_COLOR,
+            CardType.SKILL,
+            CardRarity.RARE,
+            CardTarget.SELF,
+            2
+    );
+
+    public Mitosis() {
+        super(ID, info);
+
+        setExhaust(true);
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        int currentHealth = p.currentHealth;
+        int currentTempHealth = RealTempHPField.realTempHp.get(p);
+        int currentBlock = p.currentBlock;
+        addToBot(new AddRealTemporaryHPAction(p, p, currentTempHealth));
+        addToBot(new HealAction(p, p, currentHealth));
+        if (upgraded) {
+            addToBot(new GainBlockAction(p, p, currentBlock));
+        }
+    }
+}
