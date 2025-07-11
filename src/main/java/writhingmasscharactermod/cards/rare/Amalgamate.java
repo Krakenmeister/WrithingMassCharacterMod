02e21de2ec2b857b5454a5a60e2bc724fc48f30e
Krakenmeister <johndmeo@gmail.com>
2025-07-11 07:47:24 -0400

    Added Amalgamate

---
+++ src/main/java/writhingmasscharactermod/cards/rare/Amalgamate.java
@@ -0,0 +1,35 @@
+package writhingmasscharactermod.cards.rare;
+
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import writhingmasscharactermod.actions.IncreasePlayerMaxHpAction;
+import writhingmasscharactermod.cards.BaseCard;
+import writhingmasscharactermod.character.WrithingMassCharacter;
+import writhingmasscharactermod.util.CardStats;
+
+public class Amalgamate extends BaseCard {
+    public static final String ID = makeID("Amalgamate");
+    private static final CardStats info = new CardStats(
+            WrithingMassCharacter.Meta.CARD_COLOR,
+            CardType.SKILL,
+            CardRarity.RARE,
+            CardTarget.SELF,
+            3
+    );
+
+    private static final int MAGIC_NUMBER = 8;
+    private static final int UPG_MAGIC_NUMBER = 3;
+
+    public Amalgamate() {
+        super(ID, info);
+
+        setMagic(MAGIC_NUMBER, UPG_MAGIC_NUMBER);
+
+        setExhaust(true);
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot(new IncreasePlayerMaxHpAction(p, magicNumber, true));
+    }
+}
